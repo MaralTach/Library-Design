5c42422d46e76931d56e7b214c228dc2cb50dd76
MaralTach <yoloten.maral@gmail.com>
2022-11-09 23:37:29 +0300

    blogs section

--- script.js
+++ script.js
@@ -108,6 +108,7 @@ var swiper = new Swiper(".books-slider", {
 
   var swiper = new Swiper(".reviews-slider", {
     spaceBetween: 10,
+    grabCursor:true,
     loop:true,
     centeredSlides: true,
     autoplay: {
